ed5479e2c11c30af91482ade7d7ba9b81ccabc76
Jorge Cruz <j.alexcruz@gmail.com>
2025-08-05 20:24:37 -0300

    update styles

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,26 @@
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import { View, Text, StyleSheet, StatusBar, TouchableOpacity, Image } from 'react-native';
 
 export default function App() {
  return (
    <View style={styles.container}>
     <StatusBar hidden={true} />
-        <View style={{ flex:1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text style={styles.title}>We just create an app with internacional language.</Text>
+        <View style={styles.logoView}>
+            <Image source={require('../assets/icon.png')} style={styles.logo} />
+        </View>
+        <View>
+            <Text style={styles.slogan}>The Power of React Native</Text>
+            <Text style={styles.title}>We're just creating an app.</Text>
+        </View>
+        <View style={styles.btView}>
+            <TouchableOpacity>
+                <Image source={require('../assets/brazil.png')} style={styles.flag} />
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Image source={require('../assets/usa.png')} style={styles.flag} />
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Image source={require('../assets/spain.png')} style={styles.flag} />
+            </TouchableOpacity>
         </View>
    </View>
   );
@@ -14,7 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#1d1c1c',
+    justifyContent: 'center',
+    backgroundColor: '#000',
     paddingHorizontal: 20,
   },
   title:{
@@ -22,5 +38,35 @@ const styles = StyleSheet.create({
     fontSize:58,
     fontWeight: 'bold',
     lineHeight: 60
-  }
+  },
+  slogan: {
+    color:'#fff', 
+    paddingVertical:10, 
+    fontSize:16
+},
+btView: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: '70%',
+    position: 'absolute',
+    bottom: 120,
+    left: '5%',
+},
+flag:{
+    width: 40,
+    height:40,
+    borderRadius: 20,
+},
+logoView: {
+    position: 'absolute',
+    top: 150,
+    left: '20%',
+    zIndex: 1
+},
+logo:{
+    width: 100,
+    height: 100,
+}
+
 });
